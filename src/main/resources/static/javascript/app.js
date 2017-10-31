536d6d7dca18b81cf3ba7fe38607883d5d5f61d6
Nicholas Kraftor <nkraftor@skytouchtechnology.com>
2017-10-30 19:09:57 -0700

    Modify point range

--- src/main/resources/static/javascript/app.js
+++ src/main/resources/static/javascript/app.js
@@ -53,7 +53,7 @@ PlanningPoker.controller('PokerCtrl', ['$scope', '$http', function ($scope, $htt
     $scope.defaultItemText = 'the current item';
     $scope.currentItem = $scope.defaultItemText;
     $scope.itemInput = undefined;
-    $scope.legalEstimates = [0.5, 1, 2, 3, 5, 8, 13, 20, 100];
+    $scope.legalEstimates = [0, 1, 2, 3, 4, 5];
     $scope.labels = $scope.legalEstimates.map(String);
 
     $http({
